86a1c5ab44ef2f14febfda29be3f954a70dded51
JosimarSPJunior <josimarsousapintojunior10@gmail.com>
2021-09-12 11:20:11 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSuccessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.service.SaleService;
 
 @RestController
@@ -22,5 +26,18 @@ public class SaleController {
 		Page<SaleDTO> Page = service.findAll(pageable);
 		return ResponseEntity.ok(Page);
 	}
+	
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGruoupeBySeller() {
+		List<SaleSumDTO> list = service.amountGruoupeBySeller();
+		return ResponseEntity.ok(list);
+	}
 
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGruoupeBySeller() {
+		List<SaleSuccessDTO> list = service.successGruoupeBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
 }
